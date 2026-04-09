bea6b6f734fe172bf35c42b3e4c3a55e4401a119
Nasya Sohail <mahnooranasyabila@gmail.com>
2026-04-09 18:06:27 +1000

    setup styles folder

---
+++ src/styles/spacing.ts
@@ -0,0 +1,14 @@
+export const Spacing = {
+  xs: 4,
+  sm: 10,
+  md: 14,
+  lg: 18,
+  xl: 22,
+  xxl: 30,
+
+  screenPadding: 20,
+  cardPadding: 16,
+
+  borderRadius: 12,
+  buttonRadius: 10,
+};
